48fa3dddb942d9ebb8dd536c6e715e050e98fc17
karine sobrinho <karine.silva.sobrinho@gmail.com>
2020-10-22 19:25:31 -0300

    textInput + botoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat/chatScreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: ChatScreen(),
     );
   }
 }
